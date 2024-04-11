3b476b770d8f83f3bd54deccffb1a3fff8021186
RekenGit <rutokuba@gmail.com>
2024-04-12 00:03:36 +0200

    Add: side menu, scroll bar. Fix: Background image, Move info-view elements to contact-view.

--- menu.js
+++ menu.js
@@ -1,16 +1,14 @@
 /*$(function() {
     setTimeout(() => {
-        $('#menu-icon')
-            .someCoolFunction()
-            .anotherCoolFunction();
+
     }, Math.floor(Math.random() * 50000) + 30000); //30sec to 50sec
 });*/
 
-window.onscroll = function (e) {
+/*window.onscroll = function (e) {
     let logo = $("#menu-icon");
     if (window.scrollY > 260) logo.addClass("menu-icon_hidden_bg");
     else logo.removeClass("menu-icon_hidden_bg");
-}
+}*/
 
 $("#menu-icon").on("click", function () {
     $("#menu-bar").removeClass("menu-bar-hidden");
