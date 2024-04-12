cf2b9efac454c551a7ef7af2101de8cfba45eebf
RekenGit <rutokuba@gmail.com>
2024-04-13 00:54:37 +0200

    Add galery section with animation title, Fix css, Make js code more universal.

--- menu.js
+++ menu.js
@@ -10,10 +10,15 @@ window.onscroll = function (e) {
     else logo.removeClass("menu-icon_hidden_bg");*/
 
     let scissorsIcon = $(".scissors i");
+    let scissorsPosY = $(".scissors hr").position().top - 220;
     let scrollY = window.scrollY;
-    if (scrollY > 832 && scrollY < 1552) {
-        scissorsIcon.css("left", `${(1552 - scrollY) / 8}%`);
+    if (scrollY > scissorsPosY - 720 && scrollY < scissorsPosY) {
+        scissorsIcon.css("left", `${(scissorsPosY - scrollY) / 8}%`);
     }
+
+    let galleryposY = $("#galery-view").position().top - 450;
+    if (scrollY > galleryposY) $("#galery-view-title").addClass("galery-title-popup");
+    else $("#galery-view-title").removeClass("galery-title-popup");
 }
 
 $("#menu-icon").on("click", function () {
