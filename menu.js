752db95429155c76bd145bd6c924466bd88f6535
RekenGit <rutokuba@gmail.com>
2024-04-12 16:35:59 +0200

    Added scroll scissors to contact-view

--- menu.js
+++ menu.js
@@ -4,11 +4,17 @@
     }, Math.floor(Math.random() * 50000) + 30000); //30sec to 50sec
 });*/
 
-/*window.onscroll = function (e) {
-    let logo = $("#menu-icon");
+window.onscroll = function (e) {
+    /*let logo = $("#menu-icon");
     if (window.scrollY > 260) logo.addClass("menu-icon_hidden_bg");
-    else logo.removeClass("menu-icon_hidden_bg");
-}*/
+    else logo.removeClass("menu-icon_hidden_bg");*/
+
+    let scissorsIcon = $(".scissors i");
+    let scrollY = window.scrollY;
+    if (scrollY > 832 && scrollY < 1552) {
+        scissorsIcon.css("left", `${(1552 - scrollY) / 8}%`);
+    }
+}
 
 $("#menu-icon").on("click", function () {
     $("#menu-bar").removeClass("menu-bar-hidden");
